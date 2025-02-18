c3d4b07cb3c32d4c29f1dce146dc257ebba1ac8f
Colin MacLeod <colin@bitquirky.io>
2025-02-18 14:32:52 -0600

    Still trying to get the logo to work

--- bin/tail-log.sh
+++ bin/tail-log.sh
@@ -2,6 +2,10 @@
 READLINK=$(command -v greadlink || command -v readlink)
 SCRIPT_PATH="$( $READLINK -m "$(dirname "$0")" )"
 cd $SCRIPT_PATH/.. || exit -1
+export SSH_USER=root
+export SSH_HOST=activepieces.lan
+export DOCKER_PATH=/opt/activepieces
+export DOCKER_CONTAINER=activepieces
 
-ssh root@activepieces.lan 'cd /opt/activepieces;su activepieces -s /bin/bash -c "docker compose logs -n 500 activepieces"' > errorlog.txt 2>&1
+ssh $SSH_USER@$SSH_HOST 'cd $SSH_PATH;su $SSH_CONTAINER -s /bin/bash -c "docker compose logs -n 500 $SSH_CONTAINER"' > errorlog.txt 2>&1
 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { createPiece } from '@activepieces/pieces-framework';
 import { gotoConnectAuth } from './lib/auth';
 import { sendSms } from './lib/actions/send-sms';
-import { GOTO_CONNECT_ICON } from './lib/common';
+import { GOTO_CONNECT_LOGO } from './lib/common/logo';
 
 export const gotoConnect = createPiece({
     displayName: 'GoTo Connect',
     minimumSupportedRelease: '0.1.26',
-    logoUrl: GOTO_CONNECT_ICON,
+    logoUrl: GOTO_CONNECT_LOGO,
     authors: ['Colin MacLeod'],
     auth: gotoConnectAuth,
     actions: [sendSms],
